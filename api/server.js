00ce09045c1b2bc50c077b62ddf21fce024c7b97
Paradoxrc <dinithminindu02@gmail.com>
2024-11-23 03:44:42 +0530

    z

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 
 
 // Load environment variables from .env file
